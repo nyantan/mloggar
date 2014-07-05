58f3a3c4acdf6f46df48550c11d12345268601dd
nyantan <bilibilinyan@gmail.com>
2014-07-06 01:04:15 +0800

    minor amendements

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "<html><body>";
+
+echo '<form action="fetchjson.php" method="post">';
+echo 'Mahjong log info: <input type="text" name="gid"><br>';
+echo '<input type="submit" value="send"></form>';
+
+echo "</body></html>";
+?>
