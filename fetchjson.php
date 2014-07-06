82913cf94a69eba8099780d0fbf1c91960434201
Yi Zhang <nyantan@live.cn>
2014-07-06 13:41:42 +0800

    minor clean up

--- fetchjson.php
+++ fetchjson.php
@@ -2,9 +2,9 @@
 $gid = htmlspecialchars($_POST["gid"]);
 $mjlog = "http://tenhou.net/5/mjlog2json.cgi?".$gid;
 $refer = "http://tenhou.net/5/log=".$gid;
-echo "<html>";
+
 $headers = array("GET ". $mjlog. " HTTP/1.1", "Host: tenhou.net", "Referer: ". $refer, "Accept: text/html, application/xhtml+xml, */*", "User-Agent: Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)");
-echo "<body>";
+
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $mjlog);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
